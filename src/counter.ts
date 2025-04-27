c0a1e70b8da251dbcd2e862c5357e04b1b345f6a
Jan T. Sott <git@idleberg.com>
2025-04-27 15:14:07 +0200

    build

--- src/counter.ts
+++ src/counter.ts
@@ -5,28 +5,28 @@ export async function updateCount(details: Electron.OnBeforeSendHeadersListenerD
   const multimatch = (await import('multimatch')).default;
 
   store.update(state => {
-    state.counter += 1;
+    state.counter++;
     state.buttonClass = 'error';
 
     switch(true) {
       case multimatch(details.url, trackingURLs.adobe).length > 0:
-        state.services.adobe.counter += 1;
+        state.services.adobe.counter++;
         break;
 
       case multimatch(details.url, trackingURLs.bugsnag).length > 0:
-        state.services.bugsnag.counter += 1;
+        state.services.bugsnag.counter++;
         break;
 
       case multimatch(details.url, trackingURLs.metrics).length > 0:
-        state.services.metrics.counter += 1;
+        state.services.metrics.counter++;
         break;
 
       case multimatch(details.url, trackingURLs.analytics).length > 0:
-        state.services.analytics.counter += 1;
+        state.services.analytics.counter++;
         break;
 
       case multimatch(details.url, trackingURLs.tagManager).length > 0:
-        state.services.tagManager.counter += 1;
+        state.services.tagManager.counter++;
         break;
 
       case multimatch(details.url, trackingURLs.matomo).length > 0:
@@ -34,27 +34,27 @@ export async function updateCount(details: Electron.OnBeforeSendHeadersListenerD
       case details.url.includes('matomo.php'):
       case details.url.includes('piwik.js'):
       case details.url.includes('piwik.php'):
-        state.services.matomo.counter += 1;
+        state.services.matomo.counter++;
         break;
 
       case multimatch(details.url, trackingURLs.microsoft).length > 0:
-        state.services.microsoft.counter += 1;
+        state.services.microsoft.counter++;
         break;
 
       case multimatch(details.url, trackingURLs.posthog).length > 0:
-        state.services.posthog.counter += 1;
+        state.services.posthog.counter++;
         break;
 
       case multimatch(details.url, trackingURLs.sentry).length > 0:
-        state.services.sentry.counter += 1;
+        state.services.sentry.counter++;
         break;
 
       case multimatch(details.url, trackingURLs.yandex).length > 0:
-        state.services.yandex.counter += 1;
+        state.services.yandex.counter++;
         break;
 
       default:
-        state.services.others.counter += 1;
+        state.services.others.counter++;
     }
 
     return state;
